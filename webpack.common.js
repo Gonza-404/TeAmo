3a9224d5586ae2b99539c419269aef60b4d4c572
Gonza <sergiogonzalosaenzlopez@gmail.com>
2026-03-07 18:52:57 -0500

    Sorpresa 8 de Marzo terminada y compilada

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -9,4 +10,29 @@ module.exports = {
     clean: true,
     filename: './js/app.js',
   },
+  plugins: [
+    // 1. Tu nueva portada (El Menú Principal)
+    new HtmlWebpackPlugin({
+      template: './index.html', // El archivo fuente
+      filename: 'index.html',   // El archivo que generará Webpack
+    }),
+
+    // 2. La página del primer mes (tu página original que ya renombraste)
+    new HtmlWebpackPlugin({
+      template: './mes1.html',
+      filename: 'mes1.html',
+    }),
+
+    // 3. La página del segundo mes (para el contador y las fotos)
+    new HtmlWebpackPlugin({
+      template: './mes2.html',
+      filename: 'mes2.html',
+    }),
+
+    // 4. La página sorpresa de su cumpleaños
+    new HtmlWebpackPlugin({
+      template: './cumple.html',
+      filename: 'cumple.html',
+    }),
+  ],
 };
